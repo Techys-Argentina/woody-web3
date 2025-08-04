2c11e3b61d7b815d5f43d9b60530b004cc2465c5
Techys-Argentina <guillermo.vila@techys.com.ar>
2025-08-03 22:48:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
+  base: './', // <--- clave para que se mantengan las rutas relativas
   server: {
     port: 5173
   },
