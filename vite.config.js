945f7d9c907be715cb46eab86ed3c04a28891551
Techys-Argentina <guillermo.vila@techys.com.ar>
2025-08-03 22:52:11 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   root: '.',
-  base: './', // <--- clave para que se mantengan las rutas relativas
+  base: './',
   server: {
     port: 5173
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true
-  }
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        { src: 'js', dest: '.' },
+        { src: 'lib', dest: '.' },
+        { src: 'media', dest: '.' }
+      ]
+    })
+  ]
 });
